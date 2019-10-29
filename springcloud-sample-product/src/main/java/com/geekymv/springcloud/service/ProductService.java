f4b94ac56ab7202f3ae84fc1512bb4d54fa4d5a7
geekymv <ym2011678@foxmail.com>
2019-10-29 22:15:52 +0800

    add feign and zuul

--- springcloud-sample-product/src/main/java/com/geekymv/springcloud/service/ProductService.java
+++ springcloud-sample-product/src/main/java/com/geekymv/springcloud/service/ProductService.java
@@ -1,8 +1,28 @@
 package com.geekymv.springcloud.service;
 
+import com.geekymv.springcloud.api.ProductApi;
+import com.geekymv.springcloud.mapper.ProductMapper;
 import com.geekymv.springcloud.model.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public interface ProductService {
+import javax.annotation.Resource;
 
-    Product findProductById(Long productId);
+@Slf4j
+@RestController
+@RequestMapping("/product")
+public class ProductService implements ProductApi {
+
+    @Resource
+    private ProductMapper productMapper;
+
+    @GetMapping("/detail/{productId}")
+    @Override
+    public Product productDetail(@PathVariable Long productId) {
+        log.info("productDetail productId = {}", productId);
+        return productMapper.findProductById(productId);
+    }
 }
